6b327d3aa31b55d89dd85c797ac9137b08344394
zara ahmadi <zatokey@gmail.com>
2020-05-10 19:40:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(array1,array2)
   array1.concat(array2)
 end
 
-def using_insert(array1,array2)
-  array1.insert(array2)
+def using_insert(array1,element)
+  array1.insert(element)
 end
